b39fecfd71d9e5d5be94c3cc45073fd3d385e93c
Prateek Singh Chauhan <108136303+prateek1078@users.noreply.github.com>
2023-08-20 00:25:58 +0530

    Time: 2 ms (47.41%), Space: 40.1 MB (95.18%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -1,23 +1,14 @@
-class Solution {
+public class Solution {
     public boolean isSubsequence(String s, String t) {
+        int sPointer = 0, tPointer = 0;
         
-        //converted to a character array
-        char[] sub = s.toCharArray();
-        char[] string = t.toCharArray();
-        
-        int i=0;
-        int j=0;
-        
-        //now using pointer approach, increasing i if its a match, else j
-        while(i<sub.length && j<string.length){
-            if(sub[i]==string[j]){
-                i++;
+        while (sPointer < s.length() && tPointer < t.length()) {
+            if (s.charAt(sPointer) == t.charAt(tPointer)) {
+                sPointer++;
             }
-          j++;
+            tPointer++;
         }
         
-        //if i e
-        return i==sub.length;
-        
+        return sPointer == s.length();
     }
 }
